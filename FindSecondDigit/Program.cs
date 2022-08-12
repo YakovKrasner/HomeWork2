ff3bfad390243dfbee34168ad68969656ab1097e
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-12 23:47:49 +0700

    Find Second Digit

---
+++ FindSecondDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int findSecondDigit(int arg1)
+{
+     int result = (arg1 % 100) / 10;
+     return result;
+}
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine (findSecondDigit (number));
